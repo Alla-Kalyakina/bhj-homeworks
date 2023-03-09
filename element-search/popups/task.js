9c6f29a8c822a69be80ede9cc70262fec5ec0b49
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-03-09 21:07:09 +0300

    edit task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,14 +1,16 @@
 const modalMain = document.getElementById('modal_main');
 modalMain.classList.add('modal_active');
 
-const modalClose = document.getElementsByClassName('modal__close');
-modalClose[0].onclick = function() {
-    modalMain.classList.remove('modal_active');
-    modalMain.classList.add('modal__close');
-}
+const modalClose = document.querySelectorAll('.modal');
+modalClose.forEach(item => item.onclick = function () {
+    const currentModalWindow = item.closest('div');
+    currentModalWindow.classList.remove('modal_active');
+    currentModalWindow.classList.add('modal__close');
+});
 
-const showSuccess = document.getElementsByClassName('show-success');
-showSuccess[0].onclick = function() {
+const showSuccess = document.querySelector('.show-success');
+showSuccess.onclick = function() {
     const modalSuccess = document.getElementById('modal_success');
+    modalMain.classList.remove('modal_active');
     modalSuccess.classList.add('modal_active');
-}
+};
