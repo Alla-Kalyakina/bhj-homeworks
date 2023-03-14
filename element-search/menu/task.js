2c321c83f2d4b8d263b9dd5f092cbb5096455f70
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-03-14 13:32:31 +0300

    edit task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,17 @@
 const menuLinks = document.querySelectorAll('.menu__link');
 const menuSubs = document.querySelectorAll('.menu_sub');
+let click;
 
 menuLinks.forEach(item => item.onclick = function () {
     const parentElementMenuSub = item.parentElement.querySelector(".menu_sub");
-    if (parentElementMenuSub) {
-        menuSubs.forEach(element => element.classList.remove("menu_active"));
+    if (parentElementMenuSub && !click) {
+        click = true;
         parentElementMenuSub.classList.add("menu_active");
         return false;
-      }
+        }
+    if(parentElementMenuSub && click) {
+        click = false;
+        menuSubs.forEach(element => element.classList.remove("menu_active"));
+        return false;
+    }
 });
