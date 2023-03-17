97366815a5b471351a78457b0fb01830ab16d551
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-03-17 19:52:53 +0300

    edit task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,10 +1,5 @@
 const cookie = document.getElementById("cookie");
 cookie.onclick =function() {    
         const clicker__counter = document.getElementById("clicker__counter");
-        clicker__counter.textContent++;
-        if(Number(clicker__counter.textContent) % 2) {
-                cookie.width = 300;
-        } else {
-               cookie.width = 200; 
-        }
+        cookie.width = ++clicker__counter.textContent % 2 ? 250 : 200;
 }
