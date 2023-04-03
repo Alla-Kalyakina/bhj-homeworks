692d4aba7967a0b6e54ad2dc7e6b19c9869a27a3
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-04-03 21:44:31 +0300

    add task.js

---
+++ dom/book-reader/task.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let activeElement = document.querySelector('.book');
+    const fontSizeControlBlock = document.querySelector('.book__control_font-size');
+    let fontSizes = [...fontSizeControlBlock.querySelectorAll('.font-size')];
+
+    fontSizes.forEach(element => {
+        element.addEventListener('click', setFontSize.bind(this, element));
+    });
+
+    function setFontSize(parameterA) {
+        let fontSizeActive = document.querySelector('.font-size_active');
+        fontSizeActive.classList.remove('font-size_active');
+
+        activeElement.classList.remove('book_fs-big');
+        activeElement.classList.remove('book_fs-small');
+        
+        parameterA.classList.add('font-size_active');
+
+        let size = parameterA.dataset.size;
+        activeElement.classList.add('book_fs-' + size);
+        }
+});
+
