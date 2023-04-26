2ad5616128fe1ff0a7581a52e17c759a2f98f7b1
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-04-26 13:46:28 +0300

    edit task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,13 +2,16 @@ const tooltip = [...document.querySelectorAll('.has-tooltip')];
 
 tooltip.forEach(item => {
     item.addEventListener('click', event => {
-        //event.preventDefault();
-        event.target.classList.toggle('tooltip_active');
-        if (event.target.classList.contains('tooltip_active')) {
-            event.preventDefault();
-        };
+        event.preventDefault();
         const titleText = event.target.getAttribute('title');
         const activeTargetElement = event.target.closest('.has-tooltip');
+        const tooltip = event.target.nextElementSibling;
+        if(tooltip !== null) {
+            if (tooltip.classList.contains('tooltip')) {
+                tooltip.remove();
+                return;
+            };
+        }
         const div = document.createElement('div');
   
         div.textContent = titleText;
@@ -16,6 +19,6 @@ tooltip.forEach(item => {
         const { top, left } = event.target.getBoundingClientRect();
         div.style.left = `${left}px`;
         div.style.top = `${top + 20}px`;
-        activeTargetElement.append(div);
+        activeTargetElement.after(div);
     });
 })
